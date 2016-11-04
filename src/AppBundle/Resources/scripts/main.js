5efce040321e6a5b009862e13254985c761a379d
Danielius Strašunskis <danielius55@yahoo.com>
2016-11-04 21:03:10 +0200

    javascrip time, animation fix

--- src/AppBundle/Resources/scripts/main.js
+++ src/AppBundle/Resources/scripts/main.js
@@ -8,3 +8,12 @@ $(document).ready(function() {
         });
     });
 });
+
+
+var sec = 0;
+function pad ( val ) { return val > 9 ? val : "0" + val; }
+setInterval( function(){
+    $("#seconds").html(pad(++sec%60));
+    $("#minutes").html(pad(parseInt(sec/60,10)));
+    $("#hours").html(pad(parseInt(sec/3600,10)))
+}, 1000);
