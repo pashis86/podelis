98f0600c656bfe9df5123f34edb1628126597c36
Danielius Strašunskis <danielius55@yahoo.com>
2016-11-06 15:20:30 +0200

    homepage animations, cover and buttons fix

---
+++ src/AppBundle/Resources/scripts/basic.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+
+    function scrollWin() {
+        //window.scrollTo(0, $(".formCategories").offset().top);
+        $('html, body').animate({
+                scrollTop: $('.formCategories').position().top },
+            1000
+        );
+
+    }
+
+    $(".formCategories").hide();
+
+    $(".menu-on").hide();
+
+    $(".registerForm").hide();
+
+    $(".loginForm").hide();
+
+    $( ".formButton" ).click(function() {
+
+        $( ".formCategories" ).toggle( "slow", function() {
+            scrollWin();
+            // Animation complete.
+        });
+
+    });
+
+    $( ".loginButton" ).click(function() {
+
+        $(".registerForm").hide();
+
+        $( ".loginForm" ).toggle( "slow", function() {
+            // Animation complete.
+        });
+    });
+
+    $( ".registerButton" ).click(function() {
+
+        $(".loginForm").hide();
+
+        $( ".registerForm" ).toggle( "slow", function() {
+            // Animation complete.
+        });
+    });
+});
+
+$('.myIframe').css('height', $('.myIframe').width()*9/16+'px');
+
+var sec = 0;
+function pad ( val ) { return val > 9 ? val : "0" + val; }
+setInterval( function(){
+    $("#seconds").html(pad(++sec%60));
+    $("#minutes").html(pad(parseInt(sec/60,10)));
+    $("#hours").html(pad(parseInt(sec/3600,10)))
+}, 1000);/**
+ * Created by pc on 16.11.6.
+ */
