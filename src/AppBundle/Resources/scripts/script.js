75573a5071f6870792a6d6442fb44689ec52723b
Danielius Strašunskis <danielius55@yahoo.com>
2016-11-17 16:24:00 +0200

    design

--- src/AppBundle/Resources/scripts/script.js
+++ src/AppBundle/Resources/scripts/script.js
@@ -291,7 +291,7 @@ $( document ).ready(function() {
 
 function scaleVideoContainer() {
 
-    var height = $(window).height() - 60;
+    var height = $(window).height() - 50;
     var unitHeight = parseInt(height) + 'px';
     $('.homepage-hero-module').css('height',unitHeight);
 
