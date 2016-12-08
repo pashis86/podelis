c4e153b43dcf24310e6cbd2ab4e96bcf3550d98d
Danielius Strašunskis <danielius55@yahoo.com>
2016-12-08 08:55:51 +0200

    PSR2

--- src/AppBundle/Service/TestControl.php
+++ src/AppBundle/Service/TestControl.php
@@ -110,9 +110,8 @@ class TestControl
 
     public function array_equal($a, $b)
     {
-        return (is_array($a) && is_array($b) &&
-            count($a) == count($b) &&
-            array_diff($a, $b) === array_diff($b, $a)
+        return (is_array($a) && is_array($b) && (count($a) == count($b)) &&
+            (array_diff($a, $b) === array_diff($b, $a))
         );
     }
 
