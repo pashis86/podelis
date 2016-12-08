8ac6e92057366cce72088e395e6901b60231b766
eimmar <klakalasas@gmail.com>
2016-12-08 03:13:47 +0200

    conflict resolve

--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -82,6 +82,7 @@ class TestController extends Controller
             $form = $this->createForm(TestQuestionType::class, [
                 'question' => $question, 'answered' => $session->get('answered')
             ]);
+            $form->handleRequest($request);
 
             if ($form->get('next')->isClicked()) {
                 $testControl->addAnswer($id, $form['answers']->getData());
