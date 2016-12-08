0e0b348dcc7d99df0f6428f325111554a015f4b8
Danielius Strašunskis <danielius55@yahoo.com>
2016-12-08 08:33:22 +0200

    deletion of unnecessary js

--- src/AppBundle/Resources/scripts/main.js
+++ src/AppBundle/Resources/scripts/main.js
@@ -1,31 +1,3 @@
-function timedCounter(finalValue, seconds, callback) {
-
-    var startTime = (new Date).getTime();
-    var milliseconds = seconds * 1000;
-
-    (function update() {
-
-        var currentTime = (new Date).getTime();
-        var value = finalValue * (currentTime - startTime) / milliseconds;
-
-        if (value >= finalValue)
-            value = finalValue;
-        else
-            setTimeout(update, 75);
-
-        callback && callback(value);
-
-    })();
-
-}
-
-function timedCounterCall(n) {
-    timedCounter(n, 1.5, function(value){
-        value = Math.floor(value);
-        $('.user-count').html(value);
-    });
-}
-
 function sendAnswers(inputSelector, url, question) {
     $(inputSelector).on('change', function () {
         var answers = [];
